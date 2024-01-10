2f793308f5e36441d3e26200cc261734428af3bb
Bryan Pereda <bryanpa_22@hotmail.com>
2024-01-10 18:13:06 -0500

    Eliminando Archivo WeatherForecast

--- Controllers/PagoController.cs
+++ Controllers/PagoController.cs
@@ -3,7 +3,18 @@
 namespace WebPagos;
 
 [ApiController]
+[Route("[controller]")]
 public class PagoController : ControllerBase
 {
-    
+    public PagoController()
+    {
+        
+    }
+
+    [HttpGet]
+    public ActionResult Get()
+    {
+        return Ok("Pago exitoso");
+    }
+
 }
