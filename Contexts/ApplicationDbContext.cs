0ebba02825416aaa1bbdf228ba1bb4a8b189be5e
Bryan Pereda <bryanpa_22@hotmail.com>
2024-01-16 15:54:25 -0500

    Se agregar clase ApplicationDbContext para la configuración del contexto

---
+++ Contexts/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebPagos.Contexts;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+        
+    }
+    public DbSet<Autor> Autores { get; set; }
+}
