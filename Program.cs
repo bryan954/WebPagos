51727a583caa76cf0c055ab7a8ecef10382779ae
Bryan Pereda <bryanpa_22@hotmail.com>
2024-01-16 15:53:34 -0500

    Se configura contexto para la base de datos SQLSERVER

--- Program.cs
+++ Program.cs
@@ -1,5 +1,7 @@
-var builder = WebApplication.CreateBuilder(args);
+using Microsoft.EntityFrameworkCore;
+using WebPagos.Contexts;
 
+var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
@@ -7,6 +9,8 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddDbContext<ApplicationDbContext>(options => 
+options.UseSqlServer(builder.Configuration.GetConnectionString("defaultConnection")));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
