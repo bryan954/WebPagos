070e316c84d8257d8a835ca0b6d6ae571a58481c
Bryan Pereda <bryanpa_22@hotmail.com>
2024-01-17 23:12:59 -0500

    Se agrega función AddJsonOptions para evitar los loops en las consultas

--- Program.cs
+++ Program.cs
@@ -1,10 +1,12 @@
+using System.Text.Json.Serialization;
 using Microsoft.EntityFrameworkCore;
 using WebPagos.Contexts;
 
 var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers().AddJsonOptions( x => 
+x.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
